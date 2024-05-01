12612697e3c41c1dec870d6a10e4bcaef98b4c23
NaimaAttia10 <naima.attia@insat.ucar.tn>
2024-05-01 20:27:01 +0100

    second

--- src/app.py
+++ src/app.py
@@ -1,13 +1,14 @@
 import os
 import sys
 
-sys.path.append('./Model')
+sys.path.append('./src')
+
 from flask import Flask, render_template, request, redirect, url_for
 import numpy as np
 import torch
-import cv2
+import cv2 
 
-from src.preprocess_image import preprocess_image
+from preprocess_image import preprocess_image
 
 app = Flask(__name__)
 
@@ -26,21 +27,35 @@ def predict():
         # Get user-uploaded image
         image_file = request.files['image']
 
-        # Load and preprocess the image (adapt based on your image format and model requirements)
-        image = np.fromfile(image_file, dtype=np.uint8)  # Assuming image data format
-        image = cv2.imdecode(image, cv2.IMREAD_COLOR)  # Assuming OpenCV for image processing
-        image = preprocess_image(image)  # Implement your image preprocessing logic
-        device = torch.device('cpu' )
-        model = torch.load("Model/model.pt" , map_location=device)
+        try:
+            # Load and preprocess the image (adapt based on your image format and model requirements)
+            image = np.fromfile(image_file, dtype=np.uint8)  # Assuming image data format
+            image = cv2.imdecode(image, cv2.IMREAD_COLOR)  # Assuming OpenCV for image processing
 
-        # Use your model for prediction
-        output_image = model.predict(image)  # Call your model's predict function
+            # Check if the image is successfully decoded
+            if image is None:
+                return "Erreur: Impossible de décoder l'image."
 
-        # Save the output image (optional, adjust path)
-        cv2.imwrite('output.jpg', output_image)  # Assuming OpenCV for image saving
+            image = preprocess_image(image)  # Implement your image preprocessing logic
+        except Exception as e:
+            return f"Erreur lors du prétraitement de l'image : {str(e)}"
 
-        # Display the processed image (options: show directly in the browser, redirect to a results page)
-        return render_template('results.html', output_image=output_image)  # Example for results page
+        try:
+            device = torch.device('cpu' )
+            # model = torch.load("src/model/model.pt", map_location=device)["model"]
+            model = torch.hub.load('mkang315/RCS-YOLO', 'custom', path_or_model='src/model/model.pt', force_reload=True) 
+            res = model(["https://i.imgur.com/QsYnXzR.png"])
+            res.print()
+            # Use your model for prediction
+            output_image = model.predict(image)  # Call your model's predict function
+
+            # Save the output image (optional, adjust path)
+            cv2.imwrite('output.jpg', output_image)  # Assuming OpenCV for image saving
+        
+            # Display the processed image (options: show directly in the browser, redirect to a results page)
+            return render_template('results.html', output_image=output_image)  # Example for results page
+        except Exception as e:
+            raise #return f"Erreur lors de la prédiction : {str(e)}"
     else:
         return redirect(url_for('home'))
 
